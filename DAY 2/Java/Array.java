d49d001adf3d2816dece7bb4dad1370c5b77fa88
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-05-03 15:20:25 +0530

    arrays

---
+++ DAY 2/Java/Array.java
@@ -0,0 +1,53 @@
+// Arrya decleration
+// DataType [] ArrayName  java style
+// DataType ArrayName []  c++ style
+
+// Array Initialization
+// arrayName = new type[size]
+
+// Types of arrays 
+// -- 1D Array
+// -- Multidimentional Array
+// -- jagged Array
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        
+        // int [] src={1, 2, 3, 4, 5};
+        // int [] copyarr=new int[3];
+        // System.arraycopy(src,2, copyarr, 0, 3);
+        // System.out.println(Arrays.toString(copyarr));
+
+        // int [] arr = {22, 3, 44, 55, 6, 77, 8, 99};
+        // for(int i : arr){
+        //     System.out.print(i + " ");
+        // }
+        // System.out.println();
+        // int maxi = Integer.MIN_VALUE;
+        // for(int i : arr){
+        //     maxi = Math.max(maxi, i);
+        // }
+        // System.out.println(maxi);
+
+        int [][] a = {{1, 2, 3}, {4, 5, 6}};
+        int [][] b = {{10, 20, 30}, {40, 50, 60}};
+        int [][] sum = new int[2][3];
+        for(int i = 0 ; i < 2 ; i++){
+            for(int j = 0 ; j < 3 ; j++){
+                sum[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        for(int [] i : sum){
+            for(int j : i){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+
+        
+    }
+}
