d2ec27c61413ece74f1b532e7e12e24d0edf7d06
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-05-03 12:00:42 +0530

    magic number

--- DAY 2/Java/Main.java
+++ DAY 2/Java/Main.java
@@ -7,6 +7,9 @@
 //     return getClass().getName() + "@" + integer.toHexString(hashCode());
 // }
 
+
+
+
 import java.util.Scanner;
 
 class Student {
@@ -31,16 +34,30 @@ public class Main {
         //     System.out.println(args[i]);
         // }
 
+
+
+
+
+
         // int a = 55;
         // int b = 98;
         // System.out.printf("%05d\n", a);
         // System.out.printf("%5d", b);
+
+
+
+
+
 
         // wap to accept the ascii number and print the respective character
 
         // Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
         // System.out.printf("%c", n);
+
+
+
+
 
         // Student s1 = new Student(100, "Mani");
         // Student s2 = new Student(101, "Teja");
@@ -51,34 +68,55 @@ public class Main {
         // System.out.println(s1.equals(s2));
 
 
+
+
+
         // wap to accept an number and if number is 1 print 2 and if number is 2 print 1 without using conditional statements
         // Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
         // System.out.println(3 - n);
 
 
+
+
+
         // write a program to check what type of triagle it is based on three given sides a, b, c
         // if all 3 sides are equal -- equilateral
         //  if any 2 sides are equal -- isosceles
         // if all 3 sides are unequall -- scalen
-        // A triangle is valid only id sum of any two sides are greater than teh third side
+        // A triangle is valid only id sum of any two sides are greater than the third side
+
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+
+        // if(a + b > c && b + c > a && c + a > b){
+        //     if(a == b && a == c){
+        //         System.out.println("Equilateral Triangle");
+        //     }else if(a == b || a == c || b == c){
+        //         System.out.println("Isosceles Triangle");
+        //     }else{
+        //         System.out.println("Scalene Triangle");
+        //     }
+        // }else{
+        //     System.out.println("Triangle is Not Vaild");
+        // }
+
+        // wap to detect the magic number
+        // A magic Number is one where the sum of digits are repeated until a single digit becomes 1.
+
 
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
 
-        if(a + b > c && b + c > a && c + a > b){
-            if(a == b && a == c){
-                System.out.println("Equilateral Triangle");
-            }else if(a == b || a == c || b == c){
-                System.out.println("Isosceles Triangle");
-            }else{
-                System.out.println("Scalene Triangle");
-            }
+        int n = sc.nextInt();
+
+        if((n - 1) % 9 == 0) {
+            System.out.println("Magic Number");
         }else{
-            System.out.println("Triangle is Not Vaild");
+            System.out.println("Not a magic Number");
         }
+
 
 
     }
