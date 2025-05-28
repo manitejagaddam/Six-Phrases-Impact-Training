3aebd4e3b8ee775abdcd5d01eec1c7fe754c9085
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-05-28 23:09:37 +0530

    added teh class files

---
+++ DAY 18/Class Codes/str_roatated_v1.java
@@ -0,0 +1,11 @@
+class str_roatated_v1
+{
+     public static void main(String [] args)
+	 {
+	   String s1 = "waterbottle";
+       String s2 = "bottlewater";
+       String s3 = s2 + s2;
+	   System.out.println(s3.contains(s1));
+	   
+	 }
+}
